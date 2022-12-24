bc3cf6c9a6969d6bfce59072efe77141c8aa445e
k2supra <andrijkorolevic@gmail.com>
2022-12-24 14:54:45 +0200

    done

--- Project2/main.h
+++ Project2/main.h
@@ -2,13 +2,10 @@
 using namespace std;
 
 int main() {
-	int C = 16;
-	int pi = 3.14;
-	int double_pi = 2 * pi;
-	double R = static_cast<double>(C) / double_pi;
-	int double_R = 2 * R;
-	double S = pi * double_R;
-	cout << S << endl;
-
+	int v = 5;
+	int t = 45;
+	int a = 2;
+	int s = (v * t) + ((a * (t * t)) / 2);
+	cout << s << endl;
 }
 
