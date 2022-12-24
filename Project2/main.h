d2dc13291df6b3af8e8f6011758e758f5506d0fc
k2supra <andrijkorolevic@gmail.com>
2022-12-24 14:57:32 +0200

    done

--- Project2/main.h
+++ Project2/main.h
@@ -1,5 +1,11 @@
-#pragma once
-class main
-{
-};
+#include <iostream>
+using namespace std;
+
+int main() {
+	int v = 5;
+	int t = 45;
+	int a = 2;
+	int s = (v * t) + ((a * (t * t)) / 2);
+	cout << s << endl;
+}
 
