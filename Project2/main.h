39eb430fbed3f7d9bdc079db6115c6f3118529bb
k2supra <andrijkorolevic@gmail.com>
2022-12-24 14:27:39 +0200

    done

--- Project2/main.h
+++ Project2/main.h
@@ -1,5 +1,14 @@
-#pragma once
-class main
-{
-};
+#include <iostream>
+using namespace std;
+
+int main() {
+	int C = 16;
+	int pi = 3.14;
+	int double_pi = 2 * pi;
+	double R = static_cast<double>(C) / double_pi;
+	int double_R = 2 * R;
+	double S = pi * double_R;
+	cout << S << endl;
+
+}
 
